bae76c283b71569cf2df098e9f11c0bbae32e7d1
Andrew Gerez <andrewgerez18@gmail.com>
2023-10-12 20:11:54 -0300

    feat: dynamic user id in req

--- src/main/java/br/com/andrewgerez/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/andrewgerez/todolist/filter/FilterTaskAuth.java
@@ -26,7 +26,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
 
     var servletPath = request.getServletPath();
 
-    if (servletPath.equals("/tasks")) {
+    if (servletPath.startsWith("/tasks")) {
       var authorization = request.getHeader("Authorization");
 
       var authEncoded = authorization.substring("Basic".length()).trim();
@@ -47,6 +47,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
         var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
 
         if (passwordVerify.verified) {
+          request.setAttribute("idUser", user.getId());
           filterChain.doFilter(request, response);
         } else {
           response.sendError(401);
